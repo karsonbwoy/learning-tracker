ba8c579d457b397d88da53682becc2bb475a254d
karsonbwoy <skarapyta@gmail.com>
2025-04-25 13:57:34 +0200

    initialize learning-tracker project with React, Vite, and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Dashboard from './pages/Dashboard'
+import Register from './pages/Register'
+import Login from './pages/Login'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
