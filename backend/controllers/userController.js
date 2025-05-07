29424696c8e2bb76aea7d55e3972df3b4305a4d6
karsonbwoy <skarapyta@gmail.com>
2025-05-07 21:22:13 +0200

    fix: update SameSite attribute for logout cookie to 'None' for cross-site usage

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -51,7 +51,7 @@ export const logout = async (req, res) => {
     try {
         res.clearCookie('token', {
             httpOnly: true,
-            sameSite: 'Strict',
+            sameSite: 'None',
             secure: process.env.NODE_ENV === 'production'
         });
         return res.status(200).json({ message: 'Wylogowano pomyślnie' });
