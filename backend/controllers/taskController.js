8c289c525f1e45fe0c40ae7bb812b89692bb6de2
karsonbwoy <skarapyta@gmail.com>
2025-05-21 12:33:23 +0200

    fix: remove commented-out code in getTasks function for clarity

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,8 +1,8 @@
 import Task from '../models/Task.js';
 
 export const getTasks = async (req, res) => {
-    const tasks = await Task.find({ user: req.user.id }) //tu sie wywala
-    res.json(tasks) //to sie nie wykonuje
+    const tasks = await Task.find({ user: req.user.id })
+    res.json(tasks)
 }
 
 export const createTask = async (req, res) => {
